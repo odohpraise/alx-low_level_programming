32b8de371359ce573bb2aeac33a3d3667e11eff4
odohpraise <odohpraise35@gmail.com>
2021-02-06 19:47:02 +0000

    Print out through puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio>
+/**
+ * main - Entry point ,prints Programming is like building a multlingual puzzle
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+
+}
