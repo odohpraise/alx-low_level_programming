ed2121863605c117336cf4342150bd351cb6f4a1
odohpraise <odohpraise35@gmail.com>
2021-02-10 07:55:11 +0000

    Print From base 0 to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - code block Entry
+ * Description: Program that prints base 10 to 0
+ * Return:0
+ */
+int main(void)
+{
+	int base = 0;
+
+	while (base <= 10)
+		printf("%d", base);
+	base++;
+	printf('n');
+
+	return (0);
+
+
+}
