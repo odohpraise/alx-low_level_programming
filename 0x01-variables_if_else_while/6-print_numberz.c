66ecb2c9d23a7746c46008f7dc12943431d078c4
odohpraise <odohpraise35@gmail.com>
2021-02-10 12:55:31 +0000

    print numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Code block entry
+ * Description: Print sinle digit base starting from 0-10
+ * Return:0
+ */
+int main(void)
+{
+	int base = 0;
+
+	while (base < 10)
+{
+	putchar(base);
+	base++;
+}
+	putchar('n');
+
+	return (0);
+}
