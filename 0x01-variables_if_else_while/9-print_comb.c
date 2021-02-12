72763a64f91aee462af9567709f56182a3639da7
odohpraise <odohpraise35@gmail.com>
2021-02-12 15:30:55 +0000

    Print possible combinationsinle digit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - code Block entry
+ * Description: Prints the possible combination of sinle digit
+ * numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; nu++)
+{
+	putchar(num);
+	if (num < '9')
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+	putchar('/n');
+	return (0);
+
+
+}
