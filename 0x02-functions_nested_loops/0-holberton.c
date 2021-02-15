16df3431768a9d75fc7b2257a484692bffab936e
odohpraise <odohpraise35@gmail.com>
2021-02-15 23:32:42 +0000

    Print Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * main - code block entry
+ * Description: Program that Prints holberton
+ * Return:0
+ */
+int main (void)
+{
+  char holberton;
+
+  _putchar("Holberton",\n);
+  return (0);
+
+
+
+}
