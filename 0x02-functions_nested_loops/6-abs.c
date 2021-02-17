d337d77ca65d6b6229e3a0c02c6fcc2967c2c861
odohpraise <odohpraise35@gmail.com>
2021-02-17 12:55:25 +0000

    prints absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _abs - code block entry
+ * @n: print absolute value of  integer n
+ * Return: 0
+ */
+int _abs(int n)
+{
+	if (n < 0)
+
+		return (-n);
+	else
+		return (n);
+
+}
