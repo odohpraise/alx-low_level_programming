bad5b79f062b12373b227b3244609ccddf9d0fff
odohpraise <odohpraise35@gmail.com>
2021-02-21 18:29:43 +0000

    function that adds two variables

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * add - add two integer
+ * @a : first integer
+ * @b : second integer
+ * Return: sum of two integers
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
