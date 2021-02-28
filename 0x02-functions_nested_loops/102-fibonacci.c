0f952d1bc813157af9d0d52fc8f2d7904d453e8c
odohpraise <odohpraise35@gmail.com>
2021-02-28 01:50:01 +0000

    print 50 fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,12 +10,12 @@ int main(void)
 
 	for (i = 1; i <= n; ++1)
 	{
-		printf("d, ", nextTerm);
+		printf("d, ", t1);
 		nextTerm = t1 + t2;
 		t1 = t2;
 		t2 = nextTerm;
 	}
-	printf("%d\n", nextTerm);
+
 	return (0);
 
 
