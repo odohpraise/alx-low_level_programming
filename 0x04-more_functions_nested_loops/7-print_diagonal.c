cb905e5473a713c71e8b888592886e587d825662
odohpraise <odohpraise35@gmail.com>
2021-03-05 23:31:18 +0000

    Print Diagonal Line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,15 +8,21 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
-		for (j = 0; j < n; j++)
-		{
-			if (j == i)
-				_putchar(92);
-			else
-				_putchar(' ');
-		}
 		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+
+			_putchar('\n');
+		}
 	}
 }
