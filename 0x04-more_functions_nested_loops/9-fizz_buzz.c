8d7882c29b908fee0e6cea7309e058137587a366
odohpraise <odohpraise35@gmail.com>
2021-03-06 01:30:56 +0000

    print Numbers

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - code block entry
+ * Description: print 1-100 and some string wit given multiples
+ * Return: Always Success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Fizz");
+		}
+		else if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i < 100)
+		{
+			printf(" ");
+		}
+	}
+		printf("\n");
+
+		return (0);
+
+
+}
