9f5d19473c0f50460fc2190fe7011684369bf296
odohpraise <odohpraise35@gmail.com>
2021-03-09 00:39:20 +0000

    Resets Value of pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - REsets The Value of the pointer
+ * @n: value to be reset
+ * Return: Always Success
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
