6b21ebbdfaf5d8c75d9830cc4ffb1c415b1a5a00
odohpraise <odohpraise35@gmail.com>
2021-03-12 23:51:13 +0000

    copy string

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *string_toupper - change string to upper
+ *@a: to be changeed
+ *Return: success
+ */
+char *string_toupper(char *a)
+{
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		if (a[i] >= 97 && a[i] <= 122)
+		{
+			a[i] = a[i] - 32;
+		}
+	}
+	return (a);
+}
